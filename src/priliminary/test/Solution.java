05afd166b2dd9c4d594a5a9440a785b393703c60
komal3770 <komal3770@gmail.com>
2022-04-05 12:46:26 +0530

    Added Preliminary Test Program

---
+++ src/priliminary/test/Solution.java
@@ -0,0 +1,22 @@
+package priliminary.test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Solution {
+
+  public static void main(String[] args) throws IOException {
+      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+      String n = bufferedReader.readLine();
+      BigInteger num = new BigInteger(n);
+      if(num.isProbablePrime(1))
+          System.out.println("prime");
+      else
+          System.out.println("not prime");
+
+      bufferedReader.close();
+  }
+}
